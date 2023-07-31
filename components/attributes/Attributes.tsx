12acc28b4aad3bf5ebaa7295381de3c3aea4e5ab
pdev6373 <pdev6373@gmail.com>
2023-07-31 03:36:12 +0100

    initial commit

--- components/attributes/Attributes.tsx
+++ components/attributes/Attributes.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import { Attributes as AttributesConstant } from "@/constants";
 import { Attribute } from "..";
 import styles from "./Attributes.module.css";
@@ -6,14 +7,16 @@ export default function Attributes() {
   return (
     <section className={styles.wrapper}>
       {AttributesConstant.map((attribute, index) => (
-        <Attribute
-          header={attribute.header}
-          headerTitle={attribute.headerTitle}
-          text1={attribute.text1}
-          text2={attribute.text2}
-          image={attribute.image}
-          shouldReverse={index % 2 === 1 ? true : false}
-        />
+        <Fragment key={index}>
+          <Attribute
+            header={attribute.header}
+            headerTitle={attribute.headerTitle}
+            text1={attribute.text1}
+            text2={attribute.text2}
+            image={attribute.image}
+            shouldReverse={index % 2 === 1 ? true : false}
+          />
+        </Fragment>
       ))}
     </section>
   );
